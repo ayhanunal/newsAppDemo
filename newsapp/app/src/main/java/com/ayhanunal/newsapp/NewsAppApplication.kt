349c085c3985f4af418b08fa6db66af1e9b3c50b
Ayhan Ünal <unalayhan@yahoo.com>
2021-04-24 10:41:23 +0300

    Delete newsapp directory

--- newsapp/app/src/main/java/com/ayhanunal/newsapp/NewsAppApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.ayhanunal.newsapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class NewsAppApplication : Application()
--- newsapp/app/src/main/java/com/ayhanunal/newsapp/util/Util.kt
+++
@@ -1,7 +0,0 @@
-package com.ayhanunal.newsapp.util
-
-object Util {
-
-    const val  BASE_URL = "http://www.mocky.io"
-
-}
